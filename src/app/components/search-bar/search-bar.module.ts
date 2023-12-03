7d0a13792e41cc38dbbb6fe77667f7e1216380dc
unknown <ricardoprohc@hotmail.com>
2023-12-03 13:35:04 -0300

    base do container de pesquisa

---
+++ src/app/components/search-bar/search-bar.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { SearchBarComponent } from './search-bar.component';
+
+@NgModule({
+  declarations: [SearchBarComponent],
+  exports: [SearchBarComponent]
+})
+export class SearchBarModule {}
